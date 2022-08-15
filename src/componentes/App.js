840d949ea70a66c96183837a04625ae7c55280f5
layconjohn <layconjohn@gmail.com>
2022-08-15 17:30:01 -0300

    feat: Criação de rotas

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Bitcoin from "./rotas/Bitcoin"
+import Dolar from "./rotas/Dolar"
+import Ethereum from "./rotas/Ethereum"
+import Euro from "./rotas/Euro"
+import Home from "./rotas/Home"
+
 export default function App() {
     return(
         <>
-            Hello World
+            <BrowserRouter>
+
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/dolar" element={<Dolar />}/>
+                <Route path="/euro" element={<Euro />}/>
+                <Route path="/bitcoin" element={<Bitcoin />}/>
+                <Route path="/ethereum" element={<Ethereum />}/> 
+            </Routes>
+            </BrowserRouter>
         </>
     )
 }
